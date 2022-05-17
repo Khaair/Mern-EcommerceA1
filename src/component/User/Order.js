3a9b6a21bd0aa86a8b37bce332b4ba6d64e3ef5f
khaair <khair.cse.iu@gmail.com>
2022-05-17 22:45:09 +0600

    update

--- src/component/User/Order.js
+++ src/component/User/Order.js
@@ -1,58 +1,77 @@
-import React from 'react'
+
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import './Order.css'
-import { Link } from "react-router-dom";
 
 
 export default function Order() {
-  return (
-    <div>
-      <div className='orcontainerss'>
-        <h2>Order</h2>
-        <table>
-          <tbody><tr>
-            <th>SL</th>
+
+    const [data, setData] = useState([]);
+
+    console.log(data)
+
+
+    useEffect(async () => {
+        await axios.get(`http://localhost:5000/getuserorder`, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'auth': localStorage.getItem('auth')
+            }
+        }).then(res => {
+            setData(res.data);
+            console.log(res.data,"order tracking")
+
+
+        }).catch(err => {
+            console.log(err)
+        })
+    }, [])
+
+
+   
+    return (
+        <div>
+            <div className='orcontainerss'>
+                <h2>Order</h2>
+                <table>
+                    <tbody><tr>
+                    <th>SL</th>
             <th>Order ID</th>
             <th>Status</th>
             <th>Date/Time</th>
             <th>Invoices</th>
             <th>Order Details</th>
 
-          </tr>
-            <tr>
-              <td>01</td>
-              <td>56474</td>
-              <td><button>Order</button></td>
-              <td>20.04.2022</td>
-              <td><button>Order</button></td>
-              <Link to="/ordertracking"><td><button>Check</button></td></Link>
-            </tr>
-            <tr>
-              <td>02</td>
-              <td>44899</td>
-              <td><button>Pending</button></td>
-              <td>06.05.2022</td>
-              <td><button>Order</button></td>
-              <Link to="/ordertracking"><td><button>Check</button></td></Link>
-            </tr>
-            <tr>
-              <td>03</td>
-              <td>40043</td>
-              <td><button>Order</button></td>
-              <td>20.04.2022</td>
-              <td><button>Order</button></td>
-              <Link to="/ordertracking"><td><button>Check</button></td></Link>
-            </tr>
-            <tr>
-              <td>04</td>
-              <td>39847</td>
-              <td><button>Order</button></td>
-              <td>06.05.2022</td>
-              <td><button>Order</button></td>
-              <Link to="/ordertracking"><td><button>Check</button></td></Link>
-            </tr>
-          </tbody></table>
-      </div>
 
-    </div>
-  )
+                    </tr>
+                        {data.map((el, ind) => {
+                            return (
+                                <tr key={ind}>
+                                    <td scope="row">{ind + 1}</td>
+                                    <td>{el.orderid}</td>
+                                    <td><button>Status</button></td>
+                                    <td>{el.createdAt}</td>
+
+                                    <td>
+                                    <button>make</button>
+
+
+                                    </td>
+                                    <td> 
+                                    <Link to="/ordertracking"><button>Check</button></Link>
+
+
+                                    </td>
+                                </tr>
+                            )
+
+                        })}
+
+
+                    </tbody></table>
+            </div>
+        </div>
+    )
 }
